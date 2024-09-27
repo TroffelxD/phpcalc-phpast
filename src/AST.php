6016dee7a36ca64c43dd9e39183798e74edb5934
Timo <TimoK06@t-online.de>
2024-09-28 00:32:46 +0200

    + test

--- src/AST.php
+++ src/AST.php
@@ -22,6 +22,7 @@ class AST {
         //TODO Werte in Klammern werden zuerest ausgerechnet!
         // Wurzelrechnung, Hochrechnung, ...
 
+        echo 1;
 
 
         preg_match_all($this->aPattern['getEverySign'], str_replace(' ', '', $sCalculatedString), $aMatches);
