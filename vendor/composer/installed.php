bf136029658bf7378b857d3b43bfe61981f219ce
Timo <TimoK06@t-online.de>
2024-09-28 23:38:16 +0200

    added files

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'name' => 'phpcalc/phpast',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '93eee64f87f7eb1b145961d2e76a366b34e8af42',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+        'phpcalc/phpast' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '93eee64f87f7eb1b145961d2e76a366b34e8af42',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
